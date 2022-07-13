5b9e3138e42d235281f6f8e91645a29d8b97d079
colinplancich <colin.plancich@gmail.com>
2022-07-13 13:43:10 -0700

    Updated script.js going along with Anthony's demo

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,23 @@ var apiKey = "4ab228336c684eae6edfe8b34c655dec";
 
 // Submit the form to fetch weather information
 
-    // Fetch the city name from the text <input>
-
-    // Call the 'fetchGeolocation' and pass the city name
-
 // Handle button clicks to fetch weather information
 
-    // Get the city name from the clicked button's (event.target) 
+// Fetch Geolocation Data (Geocoding API)
+function fetchGeolocation(cityName) {
 
-    // 
+    var request = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=4ab228336c684eae6edfe8b34c655dec`;
+
+    fetch(request)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+
+}
+
+fetchGeolocation("Bellingham");
+
+// Fetch Weather Data (Onecall)
