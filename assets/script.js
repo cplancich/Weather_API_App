5329e32317ef91864419131444215bcf5127d6a8
colinplancich <colin.plancich@gmail.com>
2022-07-13 10:56:38 -0700

    Added starting files, starting pseudocode & API key

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var apiKey = "4ab228336c684eae6edfe8b34c655dec";
+
+// Submit the form to fetch weather information
+
+    // Fetch the city name from the text <input>
+
+    // Call the 'fetchGeolocation' and pass the city name
+
+// Handle button clicks to fetch weather information
+
+    // Get the city name from the clicked button's (event.target) 
+
+    // 
