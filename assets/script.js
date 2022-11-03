01cbdede7cfec328bd9af74f4fc042fafc1d99b1
colinplancich <colin.plancich@gmail.com>
2022-11-03 10:38:35 -0700

    I dunno lol

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,15 @@ var apiKey = "4ab228336c684eae6edfe8b34c655dec";
 
 // Submit the form to fetch weather information
 
+    // Fetch the city name from the text <input>
+
+    // Call fetchGeolocation() and pass the city name
+
 // Handle button clicks to fetch weather information
+
+    // Get the city name from the clicked button's (event.target) data-city attribute
+
+    // Call fetchGeolocation(cityName)
 
 // Fetch Geolocation Data (Geocoding API)
 function fetchGeolocation(cityName) {
@@ -20,5 +28,24 @@ function fetchGeolocation(cityName) {
 }
 
 fetchGeolocation("Bellingham");
+fetchGeolocation("Seattle");
 
 // Fetch Weather Data (Onecall)
+function fetchOneCallWeather() {
+    
+    var lat;
+    var lon;
+
+    var request = `https://api.openweathermap.org/data/2.5/onecall?appid=4ab228336c684eae6edfe8b34c655dec&lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,minutely`
+
+    fetch(request)
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (data) {
+        console.log(data);
+        });
+
+}
+
+fetchOneCallWeather();
